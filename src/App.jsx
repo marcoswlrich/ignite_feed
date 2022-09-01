2f208c60e316c815de479f9cfc57cc0d8f0d6654
marcoswlrich <marcosapw.dev@gmail.com>
2022-08-31 23:16:26 -0400

    Sidebar init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
-import './global.css'
+import './global.css';
 
 import styles from './App.module.css'
 
